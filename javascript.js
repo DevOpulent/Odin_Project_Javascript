4340bcc6f27c35f66237b476c167a1b5b8b4eb21
Edward J. Garcia <123278174+DevOpulent@users.noreply.github.com>
2025-07-05 21:59:53 -0500

    Practicing with variables, const, numbers, and math

---
+++ javascript.js
@@ -0,0 +1,54 @@
+
+    // Your JavaScript goes here!
+    //let allows you to reassign a variable
+    let name = "Edward"
+    let surname = "Garcia"
+
+    console.log(name);
+    console.log(surname);
+
+    let age = 11;
+    console.log(age);
+
+    /*Since the age has already been declared above you can just reassign it below*/
+    age = 36;
+    console.log(age);
+
+    /*What about a constant like pi?*/
+    //const does not allow you to reassign the variable
+
+    const pi = 3.14;
+    //pi = 10;
+    console.log(pi);
+
+    //var is the older original way variable were declared until let and const superceded it.
+    
+    //Adding numbers in JavaScript can be initialized by inluding the numbers within the console.log()
+    console.log(23+97); //this will output 120 to the console
+
+    console.log(44+53+29+91+12+58); //this will output 287 to the console
+
+    console.log((4 + 6 + 9) / 77); //console will output 0.2467
+
+    //practice 1 using variables a and b and combining them.
+
+    let a = 29;
+    console.log(a)
+
+    let b = 7 * a;
+    console.log(b);
+
+    //practice 2 using const variable
+
+    const max = 57;
+    actual = max - 13;
+    percentage = actual / max;
+    console.log(percentage);
+
+    //practice 3 using speed formula speed = distance / time
+
+    let distance = 300;
+    let time = 60;
+    let speed = distance / time;
+    console.log(speed);
+
